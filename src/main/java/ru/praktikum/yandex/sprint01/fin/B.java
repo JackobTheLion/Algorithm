29a5c88a5053da74f33b6bfebd124547647d10e2
Egor Yakovlev <riddik24@yandex.ru>
2023-09-19 15:18:06 +0300

    after review

--- src/main/java/ru/praktikum/yandex/sprint01/fin/B.java
+++ src/main/java/ru/praktikum/yandex/sprint01/fin/B.java
@@ -4,30 +4,35 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
-// id посылки: 90802105
+// id посылки: 90867486
+
+//Привет, Иван!
+//Меня зовут Егор и я буду твоим студентом на этом курсе :)
+//Большое спасибо за твое ревью! С нетерпением жду продолжения.
+
 
 public class B {
 
     private static int countScore(List<String> matrix, int fingers) {
-        HashMap<Integer, Integer> numbersCount = new HashMap<>();
+        int[] numbersCount = new int[9];
 
         for (String row : matrix) {
-            String[] split = row.split("");
-            for (String s : split) {
-                if (!s.equals(".")) {
-                    Integer parsedInt = Integer.parseInt(s);
-                    numbersCount.put(parsedInt, numbersCount.getOrDefault(parsedInt, 0) + 1);
+            char[] split = row.toCharArray();
+            for (char c : split) {
+                if (c != '.') {
+                    int digit = Character.digit(c, 10);
+                    numbersCount[digit - 1] += 1;
                 }
             }
         }
 
         int result = 0;
+        int totalFingers = fingers * 2;
 
-        for (Integer integer : numbersCount.keySet()) {
-            if (numbersCount.get(integer) <= (fingers * 2)) {
+        for (int j : numbersCount) {
+            if (j <= totalFingers && j != 0) {
                 result++;
             }
         }
