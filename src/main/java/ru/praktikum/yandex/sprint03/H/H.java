be1be89c830a5f0f609ba8236862e0569bac71a1
Egor Yakovlev <riddik24@yandex.ru>
2023-09-29 15:55:16 +0300

    sprint 3 in progress

---
+++ src/main/java/ru/praktikum/yandex/sprint03/H/H.java
@@ -0,0 +1,53 @@
+package ru.praktikum.yandex.sprint03.H;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class H {
+
+    public static String getMaxNumber(List<Integer> integers, Comparator<Integer> comparator) {
+        for (int i = 1; i < integers.size(); i++) {
+            Integer intToMove = integers.get(i);
+            int j = i;
+            while (j > 0 && comparator.compare(intToMove, integers.get(j - 1)) > 0) {
+                integers.set(j, integers.get(j - 1));
+                j--;
+            }
+            integers.set(j, intToMove);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (Integer integer : integers) {
+            sb.append(integer);
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            int n = readInt(reader);
+            List<Integer> integers = readArr(reader);
+            System.out.println(getMaxNumber(integers, (Comparator<Integer>) (a, b) -> {
+                if (a.equals(b)) return 0;
+                String ab = a.toString() + b.toString();
+                String ba = b.toString() + a.toString();
+                return Integer.parseInt(ab) - Integer.parseInt(ba);
+            }));
+        }
+    }
+
+    private static int readInt(BufferedReader reader) throws IOException {
+        return Integer.parseInt(reader.readLine());
+    }
+
+    private static List<Integer> readArr(BufferedReader reader) throws IOException {
+        return Arrays.stream(reader.readLine().strip().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+}
