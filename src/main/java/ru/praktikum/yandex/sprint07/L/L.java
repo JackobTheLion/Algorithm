aa8143d0f95013869bcf2a659400df4448318a2b
Egor Yakovlev <riddik24@yandex.ru>
2023-12-20 17:30:51 +0300

    sprint 7: in progress

--- src/main/java/ru/praktikum/yandex/sprint07/L/L.java
+++ src/main/java/ru/praktikum/yandex/sprint07/L/L.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -20,24 +21,23 @@ public class L {
     }
 
     private static int calcMaxWeight(List<Integer> weights, int capacity) {
-        int[][] dp = new int[weights.size()][capacity + 1];
+        int[] dp = new int[capacity + 1];
 
-        int startWeight = weights.get(0);
-        while (startWeight <= capacity) {
-            dp[0][startWeight] = weights.get(0);
-            startWeight++;
+        for (int i = weights.get(0); i < capacity; i++) {
+            dp[i] = weights.get(0);
         }
 
         for (int i = 1; i < weights.size(); i++) {
+            int[] temp = Arrays.copyOf(dp, dp.length);
             for (int j = 0; j <= capacity; j++) {
-                int weight = j - weights.get(i);
-                if (weight >= 0) {
-                    dp[i][j] = Math.max(dp[i - 1][j], weights.get(i) + dp[i - 1][j - weights.get(i)]);
-                } else dp[i][j] = dp[i - 1][j];
+                if (j - weights.get(i) >= 0) {
+                    temp[j] = Math.max(dp[j], weights.get(i) + dp[j - weights.get(i)]);
+                }
             }
+            dp = temp;
         }
 
-        return dp[weights.size() - 1][capacity];
+        return dp[capacity];
     }
 
     private static List<Integer> readNumberOfBarsAndCapacity(BufferedReader reader) throws IOException {
