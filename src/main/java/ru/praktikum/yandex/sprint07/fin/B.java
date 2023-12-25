2f88f0ffa0acc791d6c22380447275e4facee424
Egor Yakovlev <riddik24@yandex.ru>
2023-12-26 00:36:35 +0300

    sprint 7: in progress

--- src/main/java/ru/praktikum/yandex/sprint07/fin/B.java
+++ src/main/java/ru/praktikum/yandex/sprint07/fin/B.java
@@ -3,15 +3,12 @@ package ru.praktikum.yandex.sprint07.fin;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.StringTokenizer;
 
 import static java.lang.Integer.parseInt;
 
 /* --ПРИНЦИП РАБОТЫ--
- * Принцип решения сводится к тому, чтобы привести исходную задачу к задаче о рюкзаке.
+ * Принцип решения сводится к тому, чтобы привести исходную задачу к вариации задачи о рюкзаке.
  * Пусть массив можно разделить на две части таким образом, что сумма первой части равна сумме второй. Предположим что
  * сумма всех элементов массива равна S. Тогда сумма одной половины будет равна 1/2 S. Следовательно необходимо
  * определить, возможно ли из каких то элементов массива получить сумму 1/2 S и если да, то оставшиеся тоже сложатся в
@@ -21,40 +18,39 @@ import static java.lang.Integer.parseInt;
  * запишем ее в ячейку 0 массива.
  * 2. Если сумма всех элементов не делится на 2 без остатка, значит массив разделить нельзя, возвращаем false.
  * 3. Иначе считаем, что ожидаемая сумма каждой из частей равна 1/2 S.
- * 4. Заведем массив размера 1/2 S, в котором будем хранить сумму элементов.
- * 5. Для каждого из чисел исходного массива
- * 5.2 Создаем временный массив, скопировав результирующий, в котором будем считать промежуточный результат в целях
- * оптимизации по памяти.
- * 5.2.1 Для каждой ячейки массива для подсчета суммы
- * 5.2.1.1. Если число k "помещается" в ячейку j (k <= j), то добавим в ячейку j максимальное значение, выбрав
- * между: текущего значения в ячейке j массива dp (рассчитанное на предыдущих шагах), либо сумма из числа k и числа
- * из ячейки j - k.
- * 5.3 Присвоим ссылку временного массива результирующему и повторим цикл.
- * 6. В конце цикла нам нужно сравнить число в конце результирующего массива с ожидаемой суммой. Это и будет ответ на
- * задачу.
+ * 4. Заведем массив dp булевых значений размера 1/2 S, в котором будем ходить промежуточные значения. В массиве мы
+ * будем хранить ответ на вопрос "может ли из суммы n чисел исходного массива получиться число j?", где j индекс ячейки
+ * dp.
+ * 5.1. Для каждого числа i исходного массива начиная с 1
+ * 5.1.2 Для каждой ячейки массива dp начиная с конца и до i
+ * 5.1.2.1 Проверим, может ли получится число j. Это возможно в двух случаях: если i = j, либо если есть
+ * некое число x, которое мы знаем, что можем получить (т.к. мы уже проверили его на предыдущем круге цикла) и для
+ * которого выполняется равенство x = i - j
+ * 6. Результат алгоритма будет находиться в последней ячейке массива dp.
  *
  * --ДОКАЗАТЕЛЬСТВО КОРРЕКТНОСТИ--
- * На каждом этапе мы выбираем максимальную сумму из возможных чисел, пытаясь заполнить массив-рюкзак размера 1/2 S.
- * Если нам это удается, то оставшиеся числа гарантированно будут равны 1/2 S, т.е. сумма двух подмассивов равна.
+ * 1/2 S = I + J
+ * Где 1/2 S - ожидаемая сумма, I - число исходного массива, а J - некое число, которое мы убедились, что можем
+ * получить из n чисел исходного массива.
+ * В свою очередь J = j + i, и так далее, пока i = j.
  *
  * --ВРЕМЕННАЯ СЛОЖНОСТЬ--
- * Временная сложность алгоритма составляет О(n * 1/2 S), где n - количество элементов, а S их сумма.
+ * Временная сложность алгоритма составляет О(n * 1/2 S) ~ O(n * S), где n - количество элементов, а S их сумма.
  *
  * --ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ--
- * В алгоритме используется переменная для ожидаемой суммы, которая требует О(1), результирующий массив О(1/2 S) и
- * временный массив О(1/2 S).
+ * В алгоритме используется переменная для ожидаемой суммы, которая требует О(1), результирующий массив О(1/2 S).
  *
- * Т.о. сложность составит: О(1 + 1/2 S + 1/2 S) ~ О(S).
+ * Т.о. сложность составит: О(1 + 1/2 S) ~ О(S).
  *
  */
 
-// Ссылка на посылку: https://contest.yandex.ru/contest/25597/run-report/103648174/
+// Ссылка на посылку: https://contest.yandex.ru/contest/25597/run-report/103848150/
 
 public class B {
     public static void main(String[] args) throws IOException {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             int gamesNumber = readGamesNumber(reader);
-            List<Integer> score = readScore(reader, gamesNumber);
+            int[] score = readScore(reader, gamesNumber);
 
             String result;
             if (isCanSplit(score)) {
@@ -67,39 +63,37 @@ public class B {
         }
     }
 
-    private static boolean isCanSplit(List<Integer> score) {
-        int expectedSum = score.get(0);
+    private static boolean isCanSplit(int[] score) {
+        int expectedSum = score[0];
         if (expectedSum % 2 != 0) {
             return false;
         }
         expectedSum /= 2;
 
-        int[] dp = new int[expectedSum + 1];
-        for (int i = 1; i < score.size(); i++) {
-            int[] temp = Arrays.copyOf(dp, dp.length);
-            for (int j = 0; j <= expectedSum; j++) {
-                if (j - score.get(i) >= 0) {
-                    temp[j] = Math.max(dp[j], score.get(i) + dp[j - score.get(i)]);
+        boolean[] dp = new boolean[expectedSum + 1];
+
+        for (int i = 1; i < score.length; i++) {
+            for (int j = expectedSum; j >= score[i]; j--) {
+                if (dp[j - score[i]] || j == score[i]) {
+                    dp[j] = true;
                 }
             }
-            dp = temp;
         }
 
-        return dp[expectedSum] == expectedSum;
+        return dp[expectedSum];
     }
 
     private static int readGamesNumber(BufferedReader reader) throws IOException {
         return parseInt(reader.readLine());
     }
 
-    private static List<Integer> readScore(BufferedReader reader, int gamesNumber) throws IOException {
-        List<Integer> result = new ArrayList<>(gamesNumber + 1);
-        result.add(0);
+    private static int[] readScore(BufferedReader reader, int gamesNumber) throws IOException {
+        int[] result = new int[gamesNumber + 1];
         StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
         for (int i = 1; i < gamesNumber + 1; i++) {
             int score = parseInt(tokenizer.nextToken());
-            result.add(score);
-            result.set(0, result.get(0) + score);
+            result[0] += score;
+            result[i] = score;
         }
 
         return result;
